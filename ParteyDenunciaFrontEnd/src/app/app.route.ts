90c3181733284090d2b0a267a5c241c55e264c77
Carlos Gatica <carlosgaticagalindo@gmail.com>
2022-03-18 15:56:03 -0300

    pagina home

---
+++ ParteyDenunciaFrontEnd/src/app/app.route.ts
@@ -0,0 +1,19 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { DatosgeneralesComponent } from './components/datosgenerales/datosgenerales.component';
+
+const ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'datosgenerales', component: DatosgeneralesComponent },
+
+    { path: 'buscar/:termino', component: BuscadorComponent },
+
+
+
+    { path: '**', pathMatch:'full', redirectTo: 'home' }
+];
+
+export const APPROUTING = RouterModule.forRoot(ROUTES);
