355b5787cec417de5ce894263d53355fe69e00d0
Carlos Gatica <carlosgaticagalindo@gmail.com>
2022-03-21 14:05:56 -0300

    listo pagina 3 denunciante

--- ParteyDenunciaFrontEnd/src/app/app.route.ts
+++ ParteyDenunciaFrontEnd/src/app/app.route.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 import { DatosgeneralesComponent } from './components/datosgenerales/datosgenerales.component';
+import { DenuncianteComponent } from './components/denunciante/denunciante.component';
 
 const ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'datosgenerales', component: DatosgeneralesComponent },
+    { path: 'denunciante', component: DenuncianteComponent },
 
     { path: 'buscar/:termino', component: BuscadorComponent },
 
