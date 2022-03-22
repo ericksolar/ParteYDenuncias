e5eb53ed7b56b7a736454513b151b245f272bf81
Erick Solar <ericksolarb@gmail.com>
2022-03-22 16:53:16 -0300

    Pantalla componente correccion-pantalla incompleto

--- ParteyDenunciaFrontEnd/src/app/components/funcionarios/funcionarios.component.ts
+++ ParteyDenunciaFrontEnd/src/app/components/funcionarios/funcionarios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { } from '@angular/forms';
 
 @Component({
   selector: 'app-funcionarios',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FuncionariosComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
