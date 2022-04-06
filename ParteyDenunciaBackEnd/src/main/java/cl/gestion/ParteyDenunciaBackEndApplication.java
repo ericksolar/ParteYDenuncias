899b8929b6b5fd6ab4051c1bb6921b1ee2f5f6f3
Erick Solar <ericksolarb@gmail.com>
2022-04-06 15:17:28 -0400

    UPD: Actualizacion Pantalla Victima

---
+++ ParteyDenunciaBackEnd/src/main/java/cl/gestion/ParteyDenunciaBackEndApplication.java
@@ -0,0 +1,13 @@
+package cl.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParteyDenunciaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParteyDenunciaBackEndApplication.class, args);
+	}
+
+}
---
+++ ParteyDenunciaBackEnd/src/main/java/cl/gestion/config/UpperCaseNamingStrategy.java
@@ -0,0 +1,19 @@
+package cl.gestion.config;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
+
+
+public class UpperCaseNamingStrategy extends SpringPhysicalNamingStrategy {
+    @Override
+    protected Identifier getIdentifier(String name, boolean quoted, JdbcEnvironment jdbcEnvironment) {
+        return new Identifier(UnderscoreToSnakeCase(name), quoted);
+        //return new Identifier(name.toUpperCase(), quoted);
+    }
+    
+    private String UnderscoreToSnakeCase(String id) {
+    	return id.replaceAll("_", "");
+    }
+
+}
