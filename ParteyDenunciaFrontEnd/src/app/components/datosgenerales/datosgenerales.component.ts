562e5ef8e5a58387be14b7324e16d41b5232842d
Erick Solar <ericksolarb@gmail.com>
2022-05-17 16:55:24 -0400

    Actualizacion WIP

--- ParteyDenunciaFrontEnd/src/app/components/datosgenerales/datosgenerales.component.ts
+++ ParteyDenunciaFrontEnd/src/app/components/datosgenerales/datosgenerales.component.ts
@@ -17,49 +17,57 @@ export class DatosgeneralesComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  tpDelito: any[] = [ 'ABIGEATO', 'AGRESIÓN CON LESIONES','ALTERACIÓN DEL ORDEN PÚBLICO', 'AMENAZAS', 'AROOJAR PIEDRAS U OBJETOS EN LUGARES PÚBLICOS', 'ATENTADO CONTRA LA AUTORIDAD', 'BAÑISTA TEMERARIO' , 'COHECHO', 'DAÑOS',
+  miFormulario: FormGroup;
+
+  tpDelito: any = [ 'ABIGEATO', 'AGRESIÓN CON LESIONES','ALTERACIÓN DEL ORDEN PÚBLICO', 'AMENAZAS', 'AROOJAR PIEDRAS U OBJETOS EN LUGARES PÚBLICOS', 'ATENTADO CONTRA LA AUTORIDAD', 'BAÑISTA TEMERARIO' , 'COHECHO', 'DAÑOS',
                     'FALSIFICACIÓN DE INSTRUMENTO PÚBLICO', 'FEMICIDIO', 'HOMICIDIO', 'HURTO', 'MICROTRÁFICO DE DROGAS', 'OBSTRUCCIÓN A UNA INVESTIGACIÓN', 'OMITIR REGLAS DE HIGIENE Y SALUBRIDAD EN TIEMPOS DE PANDEMIA',
                   'ORDEN JUDICIAL PENDIENTE','PENDENCIA', 'PONER EN RIESGO LA SALUD PÚBLICA' ];
 
-  tpZonaDelito: TPZonaDelito[]  = [ 
-    { CDTPZonaDelito: 1, NMZonaDelito:'Rural'},{CDTPZonaDelito: 2, NMZonaDelito:'Urbano'},{CDTPZonaDelito: 3, NMZonaDelito:'Marítimo'},
-    { CDTPZonaDelito: 4, NMZonaDelito:'Fluvial'},{CDTPZonaDelito: 5, NMZonaDelito:'Lacustre'}];
-
+  tpZonaDelito: any  = [ 'Rural','Urbano','Marítimo','Fluvial','Lacustre'];
 
   // console.log(tpZonaDelito[0]);
 
-  miFormulario: FormGroup = this.fb.group({
-    fecha: [ , [ Validators.required ] ],
-    tipoDelito: [ , [ Validators.required ] ],
-    zonaDelito: [  , [ Validators.required ] ],
-    TipoLugarOcurrencia: [ , [ Validators.required ] ],
-    lugarOcurrencia: [ , [ Validators.required ] ],
-    calle: [],
-    numero: [{disabled: true} ],
-    departamento: [],
-    block: [],
-    poblacion: [],
-    sector: [],
-    lugarEspecifico: [ , [ Validators.maxLength(3000)]],
-    region: [  , [ Validators.required ] ],
-    comuna: [  , [ Validators.required ] ],
-    referencia: [],
-    latitud: [  ,[ Validators.required ] ],
-    longitud: [  ,[ Validators.required ] ],
-  })
+  constructor(private fb: FormBuilder) {
+    this.miFormulario = this.fb.group({
+      fecha: [ , [ Validators.required ] ],
+      tipoDelito: [ , [ Validators.required ] ],
+      zonaDelito: [],
+      TipoLugarOcurrencia: [ , [ Validators.required ] ],
+      lugarOcurrencia: [ , [ Validators.required ] ],
+      calle: [],
+      numero: [{disabled: true} ],
+      departamento: [],
+      block: [],
+      poblacion: [],
+      sector: [],
+      lugarEspecifico: [ , [ Validators.maxLength(3000)]],
+      region: [  , [ Validators.required ] ],
+      comuna: [  , [ Validators.required ] ],
+      referencia: [],
+      latitud: [  ,[ Validators.required ] ],
+      longitud: [  ,[ Validators.required ] ],
+    })
+  }
 
-  constructor(private fb: FormBuilder) {}
-
-  seleccionarDelito(){}
 
   campoValido(campo: string){
     return this.miFormulario.controls[campo].errors 
             && this.miFormulario.controls[campo].touched;
   }
     
+  get TPZonaDelito(){
+    return this.miFormulario.get('zonaDelito');
+  }
 
-  onChanges(){
-    this.miFormulario.get('')
+  cambiarTPZonaDelito(e:any){
+    this.miFormulario.controls['zonaDelito'].setValue(e.target.value, {
+      onlySelf: true
+      })
+    
+  }
+
+  get TPtipoDelito(){
+    return this.miFormulario.get('tipoDelito');
   }
 
 }
